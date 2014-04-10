2b17316c8374cf8838f6214837a2966c1b7d62af
Raul Rene Lepsa <raul.lepsa@gmail.com>
2014-04-10 18:50:38 +0300

    Added mobile support for Color Match using jquery-mobile

--- public/js/games/color-match.js
+++ public/js/games/color-match.js
@@ -31,32 +31,46 @@ var colorMatch = {
         colorMatch.colorElement.addClass(colorMatch.colors[indexColor]);
     },
 
+    /* Start the game */
     start: function() {
-        var score = 0;
-        var fails = 0;
 
+        $(document).on("swipeleft", function (e) {
+            colorMatch.answer(true);
+        });
+        $(document).on("swiperight", function (e) {
+            colorMatch.answer(false);
+        });
         $(document).keydown(function (e) {
-
-            // Check matching condition: text from first div must match color in second
-            var match = colorMatch.textElement.html() === colorMatch.colorElement.attr('class');
-
-            // Remove existing classes
-            colorMatch.textElement.removeAttr('class');
-            colorMatch.colorElement.removeAttr('class');
-
-            // Check user's input. If left key was pressed and it was not a match - correct. If right key and not a match - also correct
-            if ((e.keyCode === 37 && !match) || (e.keyCode === 39 && match)) {
-                score++;
-            } else if (e.keyCode === 37 || e.keyCode === 39) {
-                fails++;
+            if (e.keyCode === 37) {
+                colorMatch.answer(true);
+            } else if (e.keyCode === 39) {
+                colorMatch.answer(false);
             }
-            
-            if (score + fails === 10) {
-                alert('Correct: ' + score + '; Incorrect: ' + fails)
-            }
-            // Populate with new values
-            colorMatch.populateElements();
         });        
-    }
+    },
+
+    /* 
+     * Detects whether the answer is correct or incorrect, and prepares the elements for the next "round".
+     * 'left' is a boolean that indicates whether the left key/swipe event has taken place. If false, it means that it was the right one
+     */
+    answer: function(left) {
+        // Check matching condition: text from first div must match color in second
+        var match = colorMatch.textElement.html() === colorMatch.colorElement.attr('class');
+        var right = !left;
+
+        // Remove existing classes
+        colorMatch.textElement.removeAttr('class');
+        colorMatch.colorElement.removeAttr('class');
+
+        // Check user's input. If left key was pressed and it was not a match - correct. If right key and not a match - also correct
+        if ((left && !match) || (right && match)) {
+            alert('Correct');
+        } else if (left || right) {
+            alert('Wrong');
+        }
+        
+        // Populate with new values
+        colorMatch.populateElements();
+    }    
 }
 
