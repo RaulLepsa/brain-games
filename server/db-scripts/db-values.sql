bc79c6be85aa4e29f5b7143fbd77e7c7dece9636
Raul Rene Lepsa <raul.lepsa@gmail.com>
2014-04-26 17:45:22 +0300

    Initial commit for integrating 2048 game

--- server/db-scripts/db-values.sql
+++ server/db-scripts/db-values.sql
@@ -3,3 +3,4 @@
 INSERT INTO games (name, category, description) VALUES ('Click it', 'Agility', 'Test how quick you can detect changes in the environment.');
 INSERT INTO games (name, category, description) VALUES ('Color match', 'Flexibility', 'Think you know the colors? What if we make it a little harder?');
 INSERT INTO games (name, category, description) VALUES ('Tiles', 'Memory', 'Improve your memory skills by trying to reproduce something you have to quickly memorize.');
+INSERT INTO games (name, category, description) VALUES ('2048', 'Puzzle', E'Join the numbers to get the 2048 tile. Sounds easy? It\'s not.');
--- server/routes/games.js
+++ server/routes/games.js
@@ -4,6 +4,8 @@
 
 var GameController = require('../controllers/game-controller');
 
+/** General routes **/
+
 // Games page
 app.get('/secure/games', function (req, res) {
 	GameController.getGamesView(req, res);
@@ -14,11 +16,6 @@ app.get('/secure/games/category/:category', function (req, res) {
 	GameController.getGamesSubview(req, res);
 });
 
-// Color Match Game page
-app.get('/secure/game/color-match', function (req, res) {
-	res.render('color-match');
-});
-
 // Save a Score for a Game
 app.post('/secure/game-score', function (req, res) {
 	GameController.saveScore(req, res);
@@ -27,4 +24,17 @@ app.post('/secure/game-score', function (req, res) {
 // Get top score for a Game
 app.get('/secure/game-score/top', function (req, res) {
 	GameController.getTopScore(req, res);
+});
+
+
+/** Specific games **/
+
+// Color Match Game page
+app.get('/secure/game/color-match', function (req, res) {
+	res.render('color-match');
+});
+
+// 2048 Game page
+app.get('/secure/game/2048', function (req, res) {
+	res.render('2048');
 });
